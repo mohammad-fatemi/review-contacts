d1b32de01bf340acfc74907c2cde8d43a0d1025e
mohammad-fatemi <mohammad.ftm.021@gmail.com>
2022-05-18 02:01:10 +0430

    the array added and elements selected in js

---
+++ app.js
@@ -0,0 +1,38 @@
+const contacts = [
+    {
+        id: 0,
+        img: "0.jpg",
+        name: "emenem",
+        job: "raper",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos assumenda iusto natusarchitecto, repudiandae incidunt necessitatibus obcaecati minima sint possimus quis pariatur! Similiquevoluptatibus ullam nisi cumque in officia alias"
+    },
+    {
+        id: 1,
+        img: "1.jpg",
+        name: "mammad",
+        job: "bikar",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos assumenda iusto natusarchitecto, repudiandae incidunt necessitatibus obcaecati minima sint possimus quis pariatur! Similiquevoluptatibus ullam nisi cumque in officia alias"
+    },
+    {
+        id: 2,
+        img: "2.jpg",
+        name: "javad",
+        job: "artist",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos assumenda iusto natusarchitecto, repudiandae incidunt necessitatibus obcaecati minima sint possimus quis pariatur! Similiquevoluptatibus ullam nisi cumque in officia alias"
+    },
+    {
+        id: 3,
+        img: "3.jpg",
+        name: "ali",
+        job: "bloger",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos assumenda iusto natusarchitecto, repudiandae incidunt necessitatibus obcaecati minima sint possimus quis pariatur! Similiquevoluptatibus ullam nisi cumque in officia alias"
+    }
+];
+
+console.log("its ok");
+
+const hisImage=document.getElementById("contact-img");
+const hisName=document.getElementById("name");
+const hisJob=document.getElementById("job");
+const hisDesc=document.getElementById("hisDesc");
+const reviewBtn=document.getElementById("reveiw-contact");
