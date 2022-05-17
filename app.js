c6fe33d80311640d03980073e020e21a55f0f552
mohammad-fatemi <mohammad.ftm.021@gmail.com>
2022-05-18 02:28:02 +0430

    the js function is added and the project is done

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const contacts = [
     },
     {
         id: 2,
-        img: "2.jpg",
+        img: "2.png",
         name: "javad",
         job: "artist",
         desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos assumenda iusto natusarchitecto, repudiandae incidunt necessitatibus obcaecati minima sint possimus quis pariatur! Similiquevoluptatibus ullam nisi cumque in officia alias"
@@ -31,8 +31,46 @@ const contacts = [
 
 console.log("its ok");
 
-const hisImage=document.getElementById("contact-img");
-const hisName=document.getElementById("name");
-const hisJob=document.getElementById("job");
-const hisDesc=document.getElementById("hisDesc");
-const reviewBtn=document.getElementById("reveiw-contact");
+const hisImage = document.getElementById("contact-img");
+const hisName = document.getElementById("name");
+const hisJob = document.getElementById("job");
+const hisDesc = document.getElementById("hisDesc");
+const reviewBtn = document.getElementById("reveiw-contact");
+
+
+reviewBtn.addEventListener("click", () => {
+    var randomNumber = Math.floor(Math.random() * (4 + 1));
+
+    console.log(randomNumber);
+    switch (randomNumber) {
+        case i = 0:
+            hisImage.setAttribute("src",contacts[0].img);
+            hisName.innerHTML = contacts[0].name;
+            hisJob.innerHTML = contacts[0].job;
+            hisDesc.innerHTML = contacts[0].desc;
+            break;
+
+        case i = 1:
+            hisImage.setAttribute("src",contacts[1].img);
+            hisName.innerHTML = contacts[1].name;
+            hisJob.innerHTML = contacts[1].job;
+            hisDesc.innerHTML = contacts[1].desc;
+            break;
+
+        case i = 2:
+            hisImage.setAttribute("src",contacts[2].img);
+            hisName.innerHTML = contacts[2].name;
+            hisJob.innerHTML = contacts[2].job;
+            hisDesc.innerHTML = contacts[2].desc;
+            break;
+
+        case i = 3:
+            hisImage.setAttribute("src",contacts[3].img);
+            hisName.innerHTML = contacts[3].name;
+            hisJob.innerHTML = contacts[3].job;
+            hisDesc.innerHTML = contacts[3].desc;
+            break;
+
+
+    }
+})
